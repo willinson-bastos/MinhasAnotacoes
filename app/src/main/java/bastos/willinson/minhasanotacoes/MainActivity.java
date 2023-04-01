b5561c547071997302b8487a84eac0f2e144110c
Willinson <willinson.bastos@gmail.com>
2023-04-01 16:16:25 -0300

    Projeto Minhas Anotações primeiro commit

---
+++ app/src/main/java/bastos/willinson/minhasanotacoes/MainActivity.java
@@ -0,0 +1,54 @@
+package bastos.willinson.minhasanotacoes;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    private AnotacaoPreferencias preferencias;
+    private EditText editAnotacao;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editAnotacao = findViewById(R.id.editAnotacao);
+
+        preferencias = new AnotacaoPreferencias(getApplicationContext());
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                //Validar se algo foi digitado
+                String textoRecuperado = editAnotacao.getText().toString();
+                if(textoRecuperado.equals("")){
+                    Snackbar.make(view, "Preencha a anotação!", Snackbar.LENGTH_LONG).show();
+                }else {
+                    preferencias.salvarAnotacao(textoRecuperado);
+                    Snackbar.make(view, "Anotação salva com sucesso!", Snackbar.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        //Recuperando anotações anteriores
+        String anotacao = preferencias.recuperarAnotacao();
+        if(!anotacao.equals("")){
+            editAnotacao.setText(anotacao);
+        }
+
+    }
+
+}
